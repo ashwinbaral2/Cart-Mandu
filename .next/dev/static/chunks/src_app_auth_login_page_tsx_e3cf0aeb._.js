0a39e9efe4c25c577e07466e675c9fc77b56a845
Ashwin Baral <140883065+ashwinbaral2@users.noreply.github.com>
2026-01-24 10:47:57 +0545

    chore: remove stray root-level next.js

--- .next/dev/static/chunks/src_app_auth_login_page_tsx_e3cf0aeb._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_109c3a54._.js",
-  "static/chunks/src_909320a0._.js"
-],
-    source: "dynamic"
-});
